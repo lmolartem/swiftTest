4f1913a37c147a91929584b7b5f0a966c1515df5
Liliana <Liliana@lilianas-mbp.lan>
2015-11-16 18:11:34 -0500

    Initial Commit

---
+++ pruebaSwiftProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaSwiftProject
+//
+//  Created by Liliana on 11/16/15.
+//  Copyright © 2015 Liliana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
